38fb2cad88116cb63f7cbc0b169fbac5b349ca59
hijiki51 <hibiki0719euph@gmail.com>
2022-02-05 02:00:26 +0900

    Create winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,9 @@
+winget install 7zip
+winget install -i git
+winget install powertoys
+winget install googlechrome
+winget install -i vscode
+winget install docker
+winget install steam
+winget install discord
+winget install slack
